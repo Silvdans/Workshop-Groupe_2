b90de6cac7dd994a5a4795545edeeb9aef509745
Nophinx <mathisroussat@gmail.com>
2022-10-11 14:04:24 +0200

    changement texte pathologie

--- accueil.php
+++ accueil.php
@@ -7,12 +7,20 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="css.css" rel="stylesheet">
-
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> 
+
+
+
+
 
     <title>Accueil</title>
 </head>
@@ -144,7 +152,146 @@
         <div class="favorites">
             <h3>Rechercher un exercice</h3>
              <ul>
-                 <li class="tooltip"><a href="" class="btn">Dysgraphie</a><span class="tooltiptext">Fonctionnalité à venir</span></li>
+                 <!-- Button trigger modal -->
+                 <button type="button" class="btn btn-primary btn-secondary rounded-15" style="background-color:  #6e6e89;" data-toggle="modal" data-target="#exampleModal">
+                    Syndrome du canal carpien
+                  </button> 
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title text-dark" id="exampleModalLongTitle">Syndrome du canal carpien</h5>
+                          <!-- <button type="button" class="Fermer" data-dismiss="modal" aria-label="Fermer">
+                            <span aria-hidden="true">&times;</span>
+                          </button> -->
+                        </div>
+                        <div class="modal-body">
+                        Le clavier, qu’il soit configuré en Azerty ou en Qwerty, ne sont pas conçus de manière ergonomique. Conjuguée à l’absence de pauses, son utilisation est responsable de douleurs dans les membres supérieurs. Les premiers symptômes se manifestent dans le cou. En fait, une mauvaise position du corps, des poignets posés sur la table sans support pour les avant-bras provoquent des tensions au niveau du canal carpien à l’origine du syndrome du même nom. En cause également : le manque de formation à la frappe et des problèmes d’ajustement au poste de travail.
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+                  <!-- Button trigger modal -->
+                 <button type="button" class="btn btn-primary btn-secondary rounded-15" style="background-color:  #6e6e89;" data-toggle="modal" data-target="#exampleModal_coude">
+                    Tendinite du coude
+                  </button>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal_coude" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title text-dark" id="exampleModalLongTitle"> Tendinite du coude</h5>
+                          <!-- <button type="button" class="Fermer" data-dismiss="modal" aria-label="Fermer">
+                            <span aria-hidden="true">&times;</span>
+                          </button> -->
+                        </div>
+                        <div class="modal-body">
+                        La manipulation de la souris sollicite les coudes, mais aussi les articulations de l’épaule si celles-ci sont continuellement relevées. À terme, une épicondylite ou tendinite du coude s’installe. Il faudra alors prendre rendez-vous avec un chirurgien spécialiste du coude dès les premiers symptômes pour éviter une intervention.
+Lombalgies
+Les positions longues et statiques au poste de travail sont la cause de douleurs dans le dos. Se lever souvent est indispensable pour soulager les tensions. Pensez à vous muscler le dos : la natation est bénéfique dans la mesure où elle ne traumatise pas le corps, le stretching également.
+Sécheresse oculaire
+Le travail sur les écrans fatigue les yeux. Au bout de quelques heures, une sensation de picotement donne l’alerte d’une sécheresse oculaire. Des lunettes spéciales sont recommandées ainsi qu’une bonne hydratation des yeux. Placez également l’ordinateur perpendiculairement à la fenêtre et adaptez l’éclairage. Préférez un écran LCD à surface mate, il évitera les reflets.
+
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+ <!-- Button trigger modal -->
+ <button type="button" class="btn btn-primary btn-secondary rounded-15" style="background-color:  #6e6e89;" data-toggle="modal" data-target="#exampleModal_cou">
+ Douleurs au cou
+
+                  </button>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal_cou" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title text-dark" id="exampleModalLongTitle">Douleurs au cou</h5>
+                          <!-- <button type="button" class="Fermer" data-dismiss="modal" aria-label="Fermer">
+                            <span aria-hidden="true">&times;</span>
+                          </button> -->
+                        </div>
+                        <div class="modal-body">
+                        Les douleurs au cou peuvent être évitées si l’écran de l’ordinateur est positionné au niveau des yeux afin de respecter la position naturelle de la nuque. La distance entre l’œil et l’écran devrait correspondre à celle d’un bras.
+
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-secondary rounded-15" style="background-color:  #6e6e89;" data-toggle="modal" data-target="#exampleModal_lambal">
+Lombalgies
+
+                  </button>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal_lambal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title text-dark" id="exampleModalLongTitle">Lombalgies</h5>
+                          <!-- <button type="button" class="Fermer" data-dismiss="modal" aria-label="Fermer">
+                            <span aria-hidden="true">&times;</span>
+                          </button> -->
+                        </div>
+                        <div class="modal-body">
+                        Les positions longues et statiques au poste de travail sont la cause de douleurs dans le dos. Se lever souvent est indispensable pour soulager les tensions. Pensez à vous muscler le dos : la natation est bénéfique dans la mesure où elle ne traumatise pas le corps, le stretching également.
+
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-secondary rounded-15" style="background-color:  #6e6e89;" data-toggle="modal" data-target="#exampleModal_secheresse">
+Sécheresse oculaire
+
+                  </button>
+
+                  <!-- Modal -->
+                  <div class="modal fade" id="exampleModal_secheresse" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title text-dark" id="exampleModalLongTitle">Sécheresse oculaire</h5>
+                          <!-- <button type="button" class="Fermer" data-dismiss="modal" aria-label="Fermer">
+                            <span aria-hidden="true">&times;</span>
+                          </button> -->
+                        </div>
+                        <div class="modal-body">
+                        Le travail sur les écrans fatigue les yeux. Au bout de quelques heures, une sensation de picotement donne l’alerte d’une sécheresse oculaire. Des lunettes spéciales sont recommandées ainsi qu’une bonne hydratation des yeux. Placez également l’ordinateur perpendiculairement à la fenêtre et adaptez l’éclairage. Préférez un écran LCD à surface mate, il évitera les reflets.
+
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+
+
+
                  <li class="tooltip"><a href="" class="btn">Dysphasie</a><span class="tooltiptext">Fonctionnalité à venir</span></li>
                  <li class="tooltip"><a href="" class="btn">Dyschronie</a><span class="tooltiptext">Fonctionnalité à venir</span></li>
                  <li class="tooltip"><a href="" class="btn">Dyspraxie</a><span class="tooltiptext">Fonctionnalité à venir</span></li>
@@ -152,11 +299,15 @@
                  <li class="tooltip"><a href="" class="btn">Troubles mnésiques</a><span class="tooltiptext">Fonctionnalité à venir</span></li>
                  <li class="tooltip"><a href="" class="btn">Dyscalculie</a><span class="tooltiptext">Fonctionnalité à venir</span></li>
                  <li class="tooltip"><a href="" class="btn">Dysorthographique</a><span class="tooltiptext">Fonctionnalité à venir</span></li>
-                 <li><a href="screen.php" class="btn">TDAH</a></li>
              </ul>
           </div>
       </div>
     </div>
+    <script>
+  $('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+  })
+</script>
 <script>
 var click = 0;
 $( "#boxmood_concentre" ).hide();
